9a4e703c35e78bbfde15ed0cb10853765526ed05
JareCoder <jesse.m.lappalainen@gmail.com>
2023-02-03 23:54:04 +0200

    init & necessary steps

---
+++ src/main/java/fi/utu/jemlap/App.java
@@ -0,0 +1,13 @@
+package fi.utu.jemlap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/fi/utu/jemlap/AppTest.java
@@ -0,0 +1,20 @@
+package fi.utu.jemlap;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
